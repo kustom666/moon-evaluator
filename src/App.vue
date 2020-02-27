5a6a6aa1dbd75b74aac94987b618498148014022
Paul Forti <paul.forti@gmail.com>
2020-02-26 23:23:48 -0600

    base

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Evaluation />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Evaluation from "./components/Evaluation.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Evaluation
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +23,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 40%;
+  margin: auto;
 }
 </style>
---
+++ src/components/Evaluation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="evaluation">
+    <h3>Moon Value Evaluation</h3>
+    <p>Just paste the result of your moon probing in the box underneath and the value of each moon will be caculated based on the market value of each ore</p>
+    <md-field>
+      <label>Moon Survey Results</label>
+      <md-textarea v-model="surveyResults"></md-textarea>
+    </md-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Evaluation",
+  data: () => {
+    return {
+      surveyResults: ""
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
